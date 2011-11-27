9c31587f03f0ca23e6dd8b66bfe689d9e4b28629
talos <irving.krauss@gmail.com>
2011-11-27 15:19:50 -0500

    eliminating extraneous start.sh whitespace

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-m2sh load -config mongrel2.conf -db the.db 
+m2sh load -config mongrel2.conf -db the.db
 m2sh start -db the.db -host localhost > log/mongrel.log 2>&1 &
 
 mongod -f mongodb.conf > log/mongodb.log 2>&1 &
