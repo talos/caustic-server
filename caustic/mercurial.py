68eb518649c428bbdfc7ce4cd1cf58199caadbeb
talos <irving.krauss@gmail.com>
2011-12-03 14:27:06 -0500

    Adding tests for mercurial package.

--- caustic/mercurial.py
+++ caustic/mercurial.py
@@ -6,53 +6,67 @@ from hgapi import hgapi
 import os
 
 # The name of the template file within the repo.
-TEMPLATE_FILE_NAME = 'template.json'
+TEMPLATE_FILE_NAME = 'content'
+
+class CommitException(Exception):
+    ''' This exception is thrown when a commit fails.
+    '''
+    pass
 
 class Repository(object):
+    ''' An object to commit, pull, and push single-file Mercurial
+    repos.
+    '''
 
-    def __init__(self, path, id):
-        ''' Checks path for a repo with the specified id.
-        If no repo exists there, creates it.  Otherwise,
+    def __init__(self, base_path, user, name):
+        ''' Checks base_path joined with the user and name for a repo.
+        If no repo exists there, creates it with the specified user.  Otherwise,
         works with the existing repo.
         '''
 
-        self.file_path = os.path.join(path, TEMPLATE_FILE_NAME)
-
-        print file_path
+        self.path = os.path.join(base_path, user, name)
+        self.user = user
+        self.file_path = os.path.join(self.path, TEMPLATE_FILE_NAME)
 
         # Create path & repo if it doesn't exist yet.
-        if(not os.path.isdir(path)):
-            os.mkdir(path)
-            self.repo = hgapi.Repo(path)
+        if(not os.path.isdir(self.path)):
+            os.makedirs(self.path)
+            self.repo = hgapi.Repo(self.path)
 
             # initialize repo
-            repo.hg_init()
+            self.repo.hg_init()
 
             # add our single file
-            with(open(self.file_path, 'w')) as f:
+            with open(self.file_path, 'w') as f:
                 f.write('')
-                repo.hg_add(self.file_path)
+
+            self.repo.hg_add(TEMPLATE_FILE_NAME)
 
         else:
-            self.repo = hgapi.Repo(path)
+            self.repo = hgapi.Repo(self.path)
 
-    def commit(self, json, message, user):
-        ''' Commits the specified JSON to this repo.
+    def commit(self, content, message):
+        ''' Commits the specified content to this repo.
         '''
 
         # Truncate & write the file.
         with open(self.file_path, 'w') as f:
-            f.write(json)
-            repo.hg_commit(message, user = user)
+            f.write(content)
+
+        try:
+            self.repo.hg_commit(message, user = self.user)
+        except Exception as e:
+            # hgapi returns a generic exception, which is sub-optimal.
+            raise CommitException(e)
 
     def pull(self, path, id):
-        ''' Pulls the JSON from the specified repo.
+        ''' Pulls the content from the specified repo.
         '''
 
         raise NotImplementedError, 'Pull not yet implemented'
 
-    def get_json(self):
-        ''' Get the latest JSON from this repo.
+    def get(self):
+        ''' Get the latest content from this repo.
         '''
 
         with open(self.file_path, 'r') as f:
--- test/mercurial.py
+++ test/mercurial.py
@@ -1,7 +1,57 @@
 '''
-Test caustic/mercurial.py
+Test caustic/mercurial.py . This won't run on Windows due to call so to
+subprocess.call()
 '''
 
+import os
+import unittest
+from subprocess import call
 from caustic.mercurial import Repository
 
-repo = Repository('test', 1)
+TEST_DIR = os.path.join('test', 'repos')
+
+class TestMercurialRepository(unittest.TestCase):
+
+    def setUp(self):
+        ''' Build a new test dir for each run.
+        '''
+        #call(['mkdir', '-p', TEST_DIR])
+        os.makedirs(TEST_DIR)
+
+    def tearDown(self):
+        ''' Kill the old test dir after each run.
+        '''
+        call(['rm', '-rf', TEST_DIR])
+
+    def test_new_repo(self):
+        ''' Create a repo.  Make sure it exists.
+        '''
+        repo = Repository(TEST_DIR, 'plato', 'foo')
+
+    def test_get_initial_content_blank(self):
+        ''' The content should be blank to start.
+        '''
+        repo = Repository(TEST_DIR, 'socrates', 'foo')
+        self.assertEqual('', repo.get())
+
+    def test_commit(self):
+        ''' Commit to the repo.  Make sure the commit happened.
+        '''
+        repo = Repository(TEST_DIR, 'hegel', 'foo')
+        repo.commit('the quick brown fox', 'committed')
+
+        self.assertEqual('the quick brown fox', repo.get())
+
+    def test_existing_repo(self):
+        ''' Retrieve a repo that already exists.
+        '''
+        repo = Repository(TEST_DIR, 'marx', 'foo')
+        repo.commit('I exist', 'committed')
+        repo = None
+
+        exists = Repository(TEST_DIR, 'marx', 'foo')
+        self.assertEqual('I exist', exists.get())
+
+# Primitive runner!
+if __name__ == '__main__':
+    unittest.main()
