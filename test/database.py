17055d9bddc1557fe3c106dba6eb9d5a1bc7787c
talos <irving.krauss@gmail.com>
2012-03-01 21:16:00 -0500

    renaming templates instructions

--- test/database.py
+++ test/database.py
@@ -1,9 +1,10 @@
-""" Test the database.  Mongod must be running.
+"""
+Test the database.  Mongod must be running.
 """
 
 import pymongo
 import unittest
-from caustic.models import User, Template
+from caustic.models import User, Instruction
 
 HOST = "http://localhost:6767"
 LOAD_GOOGLE = '{"load":"http://www.google.com/"}'
@@ -18,16 +19,16 @@ class TestDatabase(unittest.TestCase):
 
     def setUp(self):
         """Provide access to collections in test_caustic database, and easy
-        user/template fixtures.
+        user/instruction fixtures.
         """
         self.users = TestDatabase.db.users
-        self.templates = TestDatabase.db.templates
+        self.instructions = TestDatabase.db.instructions
 
     def tearDown(self):
         """Drop collections.
         """
         self.users.drop()
-        self.templates.drop()
+        self.instructions.drop()
 
     def test_save_user_assigns_id(self):
         """Saving a user should assign an id.
@@ -47,55 +48,55 @@ class TestDatabase(unittest.TestCase):
         self.users.save(User(name="sally").to_python())
         self.assertIsNotNone(self.users.find_one({'name': 'sally'}))
 
-    def test_save_template_assigns_id(self):
-        """Saving a template should assign an id.
+    def test_save_instruction_assigns_id(self):
+        """Saving a instruction should assign an id.
         """
         bobbie = User(name="bobbie")
         self.users.save(bobbie.to_python())
-        t = Template(owner=bobbie, name="template", json=LOAD_GOOGLE)
-        self.assertIsNotNone(self.templates.save(t.to_python()))
+        t = Instruction(owner=bobbie, name="instruction", json=LOAD_GOOGLE)
+        self.assertIsNotNone(self.instructions.save(t.to_python()))
 
-    def test_get_templates_by_owner_name(self):
+    def test_get_instructions_by_owner_name(self):
         """
-        Get an owner's templates.
+        Get an owner's instructions.
         """
         chris = User(name='chris')
-        self.templates.save(Template(owner=chris, name="first", json=LOAD_GOOGLE).to_python())
-        self.templates.save(Template(owner=chris, name="second", json=LOAD_GOOGLE).to_python())
-        self.assertEqual(2, self.templates.find({'owner.name': 'chris'}).count())
+        self.instructions.save(Instruction(owner=chris, name="first", json=LOAD_GOOGLE).to_python())
+        self.instructions.save(Instruction(owner=chris, name="second", json=LOAD_GOOGLE).to_python())
+        self.assertEqual(2, self.instructions.find({'owner.name': 'chris'}).count())
 
-    def test_get_templates_by_tag(self):
+    def test_get_instructions_by_tag(self):
         """
-        Get templates by their tag.
+        Get instructions by their tag.
         """
         tuten = User(name='tuten')
         mao = User(name='mao')
-        self.templates.save(Template(owner=tuten,
+        self.instructions.save(Instruction(owner=tuten,
                                      name="fiction",
                                      tags=['long march'],
                                      json=LOAD_GOOGLE).to_python())
-        self.templates.save(Template(owner=mao,
+        self.instructions.save(Instruction(owner=mao,
                                      name="works",
                                      tags=['long march'],
                                      json=LOAD_GOOGLE).to_python())
-        self.assertEqual(2, self.templates.find({'tags':'long march'}).count())
+        self.assertEqual(2, self.instructions.find({'tags':'long march'}).count())
 
-    def test_get_templates_by_tag_and_name(self):
+    def test_get_instructions_by_tag_and_name(self):
         """
-        Get templates by their tag and name.
+        Get instructions by their tag and name.
         """
         tuten = User(name='tuten')
         mao = User(name='mao')
-        self.templates.save(Template(owner=tuten,
+        self.instructions.save(Instruction(owner=tuten,
                                      name="fiction",
                                      tags=['long march'],
                                      json=LOAD_GOOGLE).to_python())
-        self.templates.save(Template(owner=mao,
+        self.instructions.save(Instruction(owner=mao,
                                      name="works",
                                      tags=['long march'],
                                      json=LOAD_GOOGLE).to_python())
-        self.assertEqual(1, self.templates.find({'tags':'long march', 'owner.name': 'mao'}).count())
-        t = Template(**self.templates.find_one({'tags':'long march', 'owner.name':'mao'}))
+        self.assertEqual(1, self.instructions.find({'tags':'long march', 'owner.name': 'mao'}).count())
+        t = Instruction(**self.instructions.find_one({'tags':'long march', 'owner.name':'mao'}))
         self.assertEqual('works', t.name)
 
 # Primitive runner!
