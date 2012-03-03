4a2079b8f6d10bb48fb49b718dadf2c72eeaf1fe
talos <irving.krauss@gmail.com>
2012-03-03 00:16:42 -0500

    updating model tests

--- test/models.py
+++ test/models.py
@@ -35,13 +35,13 @@ class TestInstruction(unittest.TestCase):
         Provide with a convenient owner and valid json.
         """
         self.owner = User(name="pwner")
-        self.json = '{"load"="http://www.google.com/"}'
+        self.instruction = {"load": "http://www.google.com/"}
 
     def test_requires_owner(self):
         """
         Should not validate without an owner
         """
-        t = Instruction(name="orphan", json=self.json)
+        t = Instruction(name="orphan", instruction=self.instruction)
         with self.assertRaises(ShieldException):
             t.validate()
 
@@ -49,24 +49,32 @@ class TestInstruction(unittest.TestCase):
         """
         Should not validate without a name.
         """
-        t = Instruction(owner=self.owner, json=self.json)
+        t = Instruction(owner=self.owner, instruction=self.instruction)
         with self.assertRaises(ShieldException):
             t.validate()
 
-    def test_requires_json(self):
+    def test_requires_instruction(self):
         """
-        Should not validate without json.
+        Should not validate without instruction
         """
         t = Instruction(owner=self.owner, name="empty")
         with self.assertRaises(ShieldException):
             t.validate()
+    
+    def test_requires_valid_instruction(self):
+        """
+        Should not validate without valid instruction.
+        """
+        i = Instruction(owner=self.owner, name='invalid', instruction={'foo':'bar'})
+        with self.assertRaises(ShieldException):
+            i.validate()
 
     def test_validates(self):
         """
-        Should validate if there is json, owner, and name.
+        Should validate if there is a valid instruction, owner, and name.
         """
-        t = Instruction(owner=self.owner, json=self.json, name="valid")
-        t.validate() # would throw an exception to fail test.
+        i = Instruction(owner=self.owner, instruction=self.instruction, name="valid")
+        i.validate() # would throw an exception to fail test.
 
     def test_is_public_by_default(self):
         """
