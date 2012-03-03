87f68c9734cfb6751b980d5e703d3578c16ff59b
talos <irving.krauss@gmail.com>
2012-03-03 02:13:54 -0500

    reorganizing handlers

--- caustic/server.py
+++ caustic/server.py
@@ -8,9 +8,10 @@ Store caustic JSON templates in little repos and let users shoot 'em round.
 
 import json
 import logging
-import bson
-from brubeck.request_handling import Brubeck, WebMessageHandler
-from brubeck.auth import authenticated, UserHandlingMixin
+import uuid
+from dictshield.base import ShieldException
+from brubeck.request_handling import Brubeck
+from brubeck.auth import UserHandlingMixin
 
 from templating import MustacheRendering
 from config     import config
@@ -53,38 +54,41 @@ class InstructionMixin():
     Use this mixin with to get and persist instructions to the database.
     """
 
-    def get_instruction(self, owner, name):
+    def _get_user(self, name):
         """
-        Get the instruction for `owner`, a User,  and `name`, a strings.
+        Get the User document for a given name, or None if there is none.
+        """
+        user = self.db_conn.users.find_one({'name': name, 'deleted': False})
+        return User(**user) if user else None
+
+    def find_instruction(self, owner, name):
+        """
+        Get the instruction for `owner` and `name`, both strings.
         Returns the Instruction if it exists, None otherwise.
         """
-        instruction = self.db_conn.instruction.find_one({
-                'owner.id': owner.id,
+        user = self._get_user(owner)
+        if not user:
+            return None
+        instruction = self.db_conn.instructions.find_one({
+                'owner.id': user.id,
                 'name': name,
                 'deleted': False })
         return Instruction(**instruction) if instruction else None
 
-    def get_tagged_instruction_names(self, owner, tag):
+    def find_tagged_instructions(self, owner, tag):
         """
-        Get all instructions for `owner`, a User, with
-        the tag String `tag`.  Returns an array of instructions,
+        Get all instructions for `owner` String with
+        the tag String `tag`.  Returns an array of Instructions,
         of 0 length if there were none for `tag` in `owner`.
         """
-        instructions = self.db_conn.templates.find({
-                    'owner.id': owner.id,
+        user = self._get_user(owner)
+        if not user:
+            return None
+        instructions = self.db_conn.instructions.find({
+                    'owner.id': user.id,
                     'tags': tag,
                     'deleted': False})
         return [Instruction(**instruction) for instruction in instructions]
-
-    def get_repo(self, instruction):
-        """
-        Return the mercurial repo for the Instruction `instruction`.
-        Will create this repo if it doesn't already exist.
-        """
-        return Repository(config['mercurial_dir'],
-                          str(instruction.owner.id),
-                          str(instruction.id))
-
 
 class JsonMixin():
     """
@@ -100,234 +104,262 @@ class JsonMixin():
 #
 # HANDLERS
 #
-class IsAliveHandler(WebMessageHandler):
-    """
-    This handler provides ping-like functionality, letting clients
-    know whether the server is available.
-    """
+class IndexHandler(MustacheRendering):
     def trace(self):
+        """
+        Trace provides ping-like functionality, letting clients
+        know whether the server is available.
+        """
         return self.render(status_code=204)
 
-
-class SignupHandler(MustacheRendering, UserMixin, JsonMixin):
-    """
-    This handler lets users sign up.  Takes argument `user`.  The
-    user is logged in after signing up.
-    """
-
     def post(self):
-        logging.warn('Signup not yet implemented.')
+        """
+        Logging in and out.
+        """
+        action = self.get_argument('action')
         context = {}
-
-        if self.current_user:
-            context['error'] = 'You are already signed up.'
-            status = 400 
-        else:
-            user = self.get_argument('user')
-
-            if not user:
-                context['error'] = 'You must specify user name to sign up'
-                status = 400
-            elif self.get_user(user):
-                context['error'] = 'User name %s is already in use.' % user
-                status = 400
+        if action == 'signup':
+            if self.current_user:
+                context['error'] = 'You are already signed up.'
+                status = 400 
             else:
-                self.save_user(user)
-                self.set_current_user(user)
-                context['user'] = user
+                user = self.get_argument('user')
+
+                if not user:
+                    context['error'] = 'You must specify user name to sign up'
+                    status = 400
+                elif self.get_user(user):
+                    context['error'] = 'User name %s is already in use.' % user
+                    status = 400
+                else:
+                    self.save_user(user)
+                    self.set_current_user(user)
+                    context['user'] = user
+                    status = 200
+        elif action == 'login':
+            logging.warn('Login not yet implemented.')
+
+            if self.current_user:
+                context['error'] = 'You are already logged in as %s' % self.current_user.name
+                status = 403
+
+            user = self.db_conn.find_one({'name': self.get_argument('user')})
+            if user:
+                self.set_current_user(User(**user))
                 status = 200
+            else:
+                context['error'] = 'User does not exist'
+                status = 403
+
+        elif action == 'logout':
+            self.logout_user()
+            status = 200
+        else:
+            context['error'] = 'Invalid action'
+            status = 400
 
         if self.is_json_request():
             self.set_body(json.dumps(context))
             self.set_status(status)
             return self.render()
         else:
-            return self.render_template('signup', _status_code=204, **context)
+            return self.render_template('user', _status_code=status, **context)
 
 
-class DeleteAccountHandler(MustacheRendering, UserMixin):
-    """
-    This handler lets a user delete his/her account.  This does not delete
-    their instructions, but will orphan them.
-    """
+class UserHandler(MustacheRendering, UserMixin, JsonMixin):
 
-    def post(self):
-        context = {}
+    def delete(self, user):
+        """
+        This handler lets a user delete his/her account.  This does not delete
+        instructions, but will orphan them.  S/he must match a deletion token.
+        """
+        context = {'user': user}
         if not self.current_user:
             context['error'] = 'You are not signed in.'
             status = 401
         else:
-            token = self.get_argument('token')
-                
-            if token:
-                self.delete_user(self.current_user)
-                return self.render(status_code=204)
+            user = self.current_user
+            token = self.get_argument('token', str(uuid.uuid4()))
+
+            if token == self.get_cookie('token', None, self.application.cookie_secret):
+                self.delete_cookie('token')
+                user.deleted = True
+                self.db_conn.save(user.to_python())
+                status = 204
+            else:
+                self.set_cookie('token', token, self.application.cookie_secret)
+                context['token'] = token
+                status = 204
 
         if self.is_json_request():
             self.set_body(json.dumps(context))
             self.set_status(status)
             return self.render()
         else:
-            return self.render_template('signup', _status_code=204, **context)
+            return self.render_template('user', _status_code=status, **context)
             
-
-
-class LogInHandler(WebMessageHandler, UserMixin):
-    """This handler lets users log in.
-    """
-
-    def post(self):
-        logging.warn('Login not yet implemented.')
-
-        if self.current_user:
-            self.set_body('You are already logged in as %s'
-                          % self.current_user.name)
-            return self.render(status_code=400)
-
-        user = self.get_user(self.get_argument('user'))
-        if not user:
-            self.set_body('User does not exist')
-            return self.render(status_code=400)
-
-        self.set_current_user(user)
-        return self.render(status_code=204)
-
-
-class LogOutHandler(WebMessageHandler, UserMixin):
-    """This handler lets users log out.
-    """
-
-    @authenticated
-    def post(self):
-        self.logout_user()
-        return self.render(status_code=204)
-
-
-class NameHandler(WebMessageHandler, UserMixin, TemplateMixin):
-    """This handler provides clients access to a single template by name.
-    """
-
-    def get(self, owner_name, name):
-        """Get the JSON of a single template.
+    def get(self, user):
         """
+        Get the user's homepage.
+        """
+        return self.render_template('user', user=user)
 
-        owner = self.get_user(owner_name)
-        if not owner:
-            self.set_body('No user %s' % owner_name)
-            return self.render(status_code=404)
 
-        template = self.get_template(owner, name)
-        if template:
-            self.set_body(template.json)
-            return self.render(status_code=200)
+class InstructionCollectionHandler(MustacheRendering, InstructionMixin, JsonMixin):
+    """
+    This handler provides access to all of a user's instructions.
+    """
+
+    def get(self, user):
+        pass
+
+    def post(self, owner_name, name):
+        """
+        Allow for cloning.
+        """
+        action = self.get_argument('action')
+        if action == 'clone':
+            template = self.get_template(self.get_user(owner_name), name)
+            if not template:
+                self.set_body('Template %s/%s does not exist.' % (owner_name, name))
+                return self.render(status_code=404)
+
+            repo_to_clone = self.get_repo(template)
+
+            template.id = None # Force mongo to create a new template.
+            self.persist_template(template) # This assigns a new ID.
+
+            repo_to_clone.clone(self.current_user.id, template.id)
+            return self.render(status_code=204)
+
+
+class TagCollectionHandler(MustacheRendering, InstructionMixin, JsonMixin):
+    """
+    This handler provides access to all of a user's instructions with a certain
+    tag.
+    """
+    def get(self, owner, tag):
+        instructions = self.find_tagged_instructions(owner, tag)
+
+        if self.is_json_request():
+            self.set_body(json.dumps(instructions))
+            return self.render()
         else:
-            self.set_body('No template')
-            return self.render(status_code=404)
+            return self.render_template('tagged', instructions=instructions)
 
-    @authenticated
-    def put(self, owner_name, name):
-        """Update a single template, creating it if it doesn't exist.
+
+class InstructionModelHandler(MustacheRendering, UserMixin, InstructionMixin, JsonMixin):
+    """
+    This handler provides clients access to a single instruction by name.
+    """
+
+    def get(self, owner, name):
         """
+        Display a single instruction.
+        """
+        context = {}
+        instruction = self.find_instruction(owner, name)
+        if instruction:
+            context['instruction'] = instruction.to_python()
+            status = 200
+        else:
+            context['error'] = "Instruction does not exist"
+            status = 404
 
-        if not owner_name == self.current_user.name:
-            self.set_body( 'This is not your template.')
-            return self.render(status_code=400)
+        if self.is_json_request():
+            # for JSON, only include the instruction proper
+            self.set_body(json.dumps(context['instruction']))
+            self.set_status(status)
+            return self.render()
+        else:
+            return self.render_template('instruction', _status_code=status, **context)
 
-        json_template = self.get_argument('json') # leave json
-                                                  # template as string
-        if not json_template:
-            self.set_body('You must specify json for the template.')
-            return self.render(status_code=400)
-        #TODO validation for template.
+    def put(self, owner, name):
+        """
+        Update a single instruction, creating it if it doesn't exist.
+        """
+        user = self.current_user 
+        context = {}
+        if not user:
+            context['error'] = "You are not logged in."    
+            status = 403
+        elif owner != user.name:
+            context['error'] = 'This is not your template.'
+            status = 403
+        else:
+            instruction = self.find_instruction(user, name) 
+            try:
+                instruction = self.find_instruction(user, name)
+                if instruction:
+                    commit = self.get_argument('commit', 'First commit')
+                else:
+                    commit = self.get_argument('commit', '')
+                    instruction = Instruction(name=name, owner=user)
 
-        tags = json.loads(self.get_argument('tags', '[]')) # load json array
-        commit = self.get_argument('commit', config['default_commit'])
+                instruction.tags = json.loads(self.get_argument('tags', '[]')),
+                instruction.instruction = json.loads(self.get_argument('instruction'))
 
-        template = self.get_template(self.current_user, name)
-        if template: # modify existing template
-            template.tags = tags
-            template.json = json_template
-        else: # new template
-            template = Template(
-                name=name,
-                tags=tags,
-                json=json_template,
-                owner=current_user)
+                repo = Repository(config['mercurial_dir'],
+                          str(instruction.owner.id),
+                          str(instruction.id))
+                repo.commit(json.dumps(instruction.instruction,
+                                       indent=4,
+                                       sort_keys=True),
+                           commit)
+                self.save_instruction(instruction)
 
-        self.persist_template(template)
+            except ShieldException as error:
+                context['error'] = "Invalid instruction: %s." % error
+                status = 400
+            except ValueError as error:
+                context['error'] = 'Invalid JSON: %s.' % error
+                status = 400
+            context['instruction'] = instruction
 
-        repo = self.get_repo(template)
-        repo.commit(json_template, commit)
+        if self.is_json_request():
+            # for JSON, only include the instruction proper
+            self.set_body(json.dumps(context['instruction']))
+            self.set_status(status)
+            return self.render()
+        else:
+            return self.render_template('instruction', _status_code=status, **context)
 
-        return self.render(status_code=204)
-
-    @authenticated
-    def delete(self, owner_name, name):
-        """Hide a single template from view.  Does not actually wipe the
+    def delete(self, owner, name):
+        """
+        Hide a single template from view.  Does not actually wipe the
         repo, but sets a 'deleted' flag for the template.
         """
-        if not owner_name == self.current_user.name:
-            self.set_body('You cannot delete someone else\'s template')
-            return self.render(status_code=400)
+        user = self.current_user
+        context = {}
+        if not user:
+            context['error'] = 'You are not logged in'
+        elif not owner == user.name:
+            context['error'] = "You cannot delete someone else's template"
+            status = 403
+        else:
+            instruction = self.find_instruction(owner, name)
+            if instruction:
+                instruction.deleted = True
+                self.db_conn.instructions.save(instruction)
+                status = 200
+            else:
+                status['error'] = "Instruction does not exist"
+                status = 404
 
-        template = self.get_template(self.current_user, name)
-
-        if not template:
-            self.set_body('No template')
-            return self.render(status_code=404)
-
-        template.deleted = True
-        self.persist_template(template)
-
-        return self.render(status_code=204)
-
-
-class CloneHandler(WebMessageHandler, UserMixin, TemplateMixin):
-    """This handler clones an existing repo for the logged in user,
-    with the same name.
-    """
-    @authenticated
-    def post(self, owner_name, name):
-        template = self.get_template(self.get_user(owner_name), name)
-        if not template:
-            self.set_body('Template %s/%s does not exist.' % (owner_name, name))
-            return self.render(status_code=404)
-
-        repo_to_clone = self.get_repo(template)
-
-        template.id = None # Force mongo to create a new template.
-        self.persist_template(template) # This assigns a new ID.
-
-        repo_to_clone.clone(self.current_user.id, template.id)
-        return self.render(status_code=204)
-
-class TagHandler(WebMessageHandler, UserMixin, TemplateMixin):
-    """This handler serves an array of template links by tag.
-    """
-    def get(self, owner_name, tag):
-        owner = self.get_user(owner_name)
-        if not owner:
-            self.set_body('User %s does not exist.' % owner_name)
-            return self.render(status_code=404)
-
-        tagged_templates = self.get_tagged_templates(owner, tag)
-
-        self.set_body(json.dumps([
-                    "../%s" % template.name for template in tagged_templates]))
-        return self.render(status_code=200)
+        if self.is_json_request():
+            self.set_body(json.dumps(context['instruction']))
+            self.set_status(status)
+            return self.render()
+        else:
+            return self.render_template('instruction', _status_code=status, **context)
 
 
 config['handler_tuples'] = [
-    (r'^/$', IsAliveHandler),
-    (r'^/signup$', SignupHandler),
-    (r'^/die$', DeleteAccountHandler),
-    (r'^/login$', LoginHandler),
-    (r'^/logout$', LogoutHandler),
-    (r'^/([\w\-]+)/tagged/([\w\-]+)/?$', TagIndexHandler),
+    (r'^/$', IndexHandler),
+    (r'^/([\w\-]})/?$', UserHandler),
     (r'^/([\w\-]+)/instructions/?$', InstructionCollectionHandler),
     (r'^/([\w\-]+)/instructions/([\w\-]+)/?$', InstructionModelHandler),
-    (r'^/([\w\-]+)/instructions/([\w\-]+)/tag/([\w\-]+)/?$', InstructionTagHandler)]
-
+    (r'^/([\w\-]+)/tagged/([\w\-]+)/?$', TagCollectionHandler)]
 
 app = Brubeck(**config)
 app.run()
