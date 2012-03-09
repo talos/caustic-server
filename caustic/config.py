16208d021e8ae0b045c9edf85f5dbe01d463b3ad
John Krauss <irving.krauss@gmail.com>
2012-03-09 17:19:23 -0500

    adding mustache load

--- caustic/config.py
+++ caustic/config.py
@@ -4,10 +4,12 @@ Configuration dict for caustic.
 
 import uuid
 import pymongo
+from brubeck.templating import load_mustache_env
 
 config = {
     'mongrel2_pair': ('ipc://127.0.0.1:9999', 'ipc://127.0.0.1:9998'),
     'db_conn'  : pymongo.Connection('localhost', 27017).caustic,
     'cookie_secret': str(uuid.uuid4()),
-    'git_dir' : 'git'
+    'template_loader': load_mustache_env('./templates'),
+    'git_dir': 'git'
 }
